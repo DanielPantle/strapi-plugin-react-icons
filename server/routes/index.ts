5a20b7d7d5e89e501b05b0158c23708639cf7529
Daniel Pantle <daniel.pantle@newtec.de>
2023-04-26 12:23:08 +0200

    Remove config from routes to enable authorization

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,36 +3,20 @@ export default [
     method: 'GET',
     path: '/iconlibrary/find',
     handler: 'iconLibraryController.find',
-    config: {
-      policies: [],
-      auth: false
-    },
   },
   {
     method: 'POST',
     path: '/iconlibrary/post',
     handler: 'iconLibraryController.create',
-    config: {
-      policies: [],
-      auth: false
-    },
   },
   {
     method: 'PUT',
     path: '/iconlibrary/update/:id',
     handler: 'iconLibraryController.update',
-    config: {
-      policies: [],
-      auth: false
-    },
   },
   {
     method: 'DELETE',
     path: '/iconlibrary/delete/:id',
     handler: 'iconLibraryController.delete',
-    config: {
-      policies: [],
-      auth: false
-    },
   },
 ];
