6d54cc09c0d8b29be7e4cf1da2779906e3620220
prestonator <preston.b.jenkins-1@ou.edu>
2023-05-22 09:37:38 -0500

    🎨 style(ReactIconsSelector): improve UI/UX of the component ✨ feat(ReactIconsSelector): add search functionality to the icon selector modal The changes made to the ReactIconsSelector component include improving the UI/UX of the component by adding a fixed button to the input field and styling the input field to be more visually appealing. The modal that displays the icons has been updated to include a search bar that allows users to search for icons by name. This makes it easier for users to find the icon they need, especially when there are many icons to choose from.

--- admin/src/components/ReactIconsSelector/index.tsx
+++ admin/src/components/ReactIconsSelector/index.tsx
@@ -1,200 +1,266 @@
 import React, { useState, useEffect } from "react";
 import {
-  Flex,
-  Button,
-  Box,
-  ModalLayout,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Typography,
-  Select,
-  FieldAction,
-  Option,
-  TextInput,
+	Flex,
+	Button,
+	Box,
+	ModalLayout,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Typography,
+	Select,
+	FieldAction,
+	Option,
 } from "@strapi/design-system";
+import { TextInput as OriginalTextInput } from "@strapi/design-system";
 import * as ReactIcons from "react-icons/all";
 import { Cross } from "@strapi/icons";
 import { SearchIcon } from "@strapi/icons";
 import { useIntl, MessageDescriptor } from "react-intl";
 import { request } from "@strapi/helper-plugin";
 import { IconContext } from "react-icons/lib";
+import { FixedSizeGrid as Grid } from "react-window";
 
 interface IReactIconsSelector {
-  description: null | MessageDescriptor;
-  intlLabel: null | MessageDescriptor;
-  placeholder: null | MessageDescriptor;
-  name: string;
-  error: string;
-  required: boolean;
-  onChange: any;
-  value: string;
+	description: null | MessageDescriptor;
+	intlLabel: null | MessageDescriptor;
+	placeholder: null | MessageDescriptor;
+	name: string;
+	error: string;
+	required: boolean;
+	onChange: any;
+	value: string;
 }
 
 interface IIconComponent {
-  icon: string;
-  size?: number;
+	icon: string;
+	size?: number;
 }
 
 type IReactIcon = keyof typeof ReactIcons;
 
 const strapiTheme = window.localStorage.STRAPI_THEME;
 
+const inputButtonStyle = {
+	background: "#0021a4",
+	position: "absolute",
+	bottom: 0,
+	width: "5rem",
+	height: "100%",
+	justifyContent: "center",
+} as React.CSSProperties;
+
+const TextInputContainer = styled.div`
+	& div[disabled] {
+		/* Styles for the child <div> with the disabled attribute */
+		/* Add your styles here */
+		position: relative;
+	}
+	& input[type="text"] {
+		width: calc(100% - 10rem);
+		text-align: center;
+	}
+`;
+
+const TextInput = ({ ...props }) => {
+	return (
+		<TextInputContainer>
+			<OriginalTextInput {...props} />
+		</TextInputContainer>
+	);
+};
+
 const IconComponent: React.FC<IIconComponent> = ({ icon, size }) => {
-  const DynamicIconComponent = ReactIcons[icon as IReactIcon];
+	const DynamicIconComponent = ReactIcons[icon as IReactIcon];
 
-  if (undefined === DynamicIconComponent) return <></>;
+	if (undefined === DynamicIconComponent) return <></>;
 
-  return (
-    <IconContext.Provider
-      value={{ color: strapiTheme === "light" ? "#212134" : "#a5a5ba" }}
-    >
-      <DynamicIconComponent size={size} />
-    </IconContext.Provider>
-  );
+	return (
+		<IconContext.Provider
+			value={{ color: strapiTheme === "light" ? "#212134" : "#a5a5ba" }}
+		>
+			<DynamicIconComponent size={size} />
+		</IconContext.Provider>
+	);
 };
 
 const ReactIconsSelector: React.FC<IReactIconsSelector> = ({
-  description,
-  error,
-  intlLabel,
-  placeholder,
-  name,
-  required,
-  onChange,
-  value,
+	description,
+	error,
+	intlLabel,
+	placeholder,
+	name,
+	required,
+	onChange,
+	value,
 }) => {
-  const { formatMessage } = useIntl();
+	const { formatMessage } = useIntl();
 
-  const [iconLibraries, setIconLibraries] = useState<IIconLibrary[]>([]);
-  const [selectedIconLibrary, setSelectedIconLibrary] = useState<string | null>(
-    null
-  );
-  const [selectableIcons, setSelectableIcons] = useState([] as IReactIcon[]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+	const [iconLibraries, setIconLibraries] = useState<IIconLibrary[]>([]);
+	const [selectedIconLibrary, setSelectedIconLibrary] = useState<string | null>(
+		null
+	);
+	const [selectableIcons, setSelectableIcons] = useState([] as IReactIcon[]);
+	const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const toggleModal = () => setIsModalVisible((prev) => !prev);
+	// Add a new state for search term
+	const [searchTerm, setSearchTerm] = useState("");
 
-  const changeIcon = (newIcon: string) =>
-    onChange({
-      target: {
-        name,
-        type: "string",
-        value: newIcon,
-      },
-    });
+	const toggleModal = () => setIsModalVisible((prev) => !prev);
 
-  useEffect(() => {
-    const getIconLibraries = async () => {
-      setIconLibraries(
-        (
-          await request("/react-icons/iconlibrary/find", {
-            method: "GET",
-          })
-        ).filter((iconLibrary: IIconLibrary) => iconLibrary.isEnabled)
-      );
-    };
+	const changeIcon = (newIcon: string) =>
+		onChange({
+			target: {
+				name,
+				type: "string",
+				value: newIcon,
+			},
+		});
 
-    getIconLibraries();
-  }, []);
+	useEffect(() => {
+		const getIconLibraries = async () => {
+			setIconLibraries(
+				(
+					await request("/strapi-react-icons/iconlibrary/find", {
+						method: "GET",
+					})
+				).filter((iconLibrary: IIconLibrary) => iconLibrary.isEnabled)
+			);
+		};
 
-  useEffect(() => {
-    selectedIconLibrary &&
-      setSelectableIcons(
-        Object.keys(ReactIcons).filter((icon) =>
-          icon.toLowerCase().startsWith(selectedIconLibrary)
-        ) as IReactIcon[]
-      );
-  }, [selectedIconLibrary, setSelectableIcons]);
+		getIconLibraries();
+	}, []);
 
-  return (
-    <>
-      <TextInput
-        type="text"
-        label={intlLabel && formatMessage(intlLabel)}
-        placeholder={placeholder && formatMessage(placeholder)}
-        hint={description && formatMessage(description)}
-        disabled={true}
-        onChange={onChange}
-        id={name}
-        name={name}
-        value={value || ""}
-        required={required}
-        error={error}
-        startAction={
-          <FieldAction onClick={toggleModal}>
-            {value ? <IconComponent icon={value} /> : <ReactIcons.TbSearch />}
-          </FieldAction>
-        }
-        endAction={
-          !!value && (
-            <FieldAction onClick={() => changeIcon("")}>
-              <ReactIcons.TbX />
-            </FieldAction>
-          )
-        }
-      />
+	useEffect(() => {
+		setSelectableIcons(
+			Object.keys(ReactIcons).filter((icon) =>
+				icon.toLowerCase()
+			) as IReactIcon[]
+		);
+	}, [selectedIconLibrary, setSelectableIcons]);
 
-      {isModalVisible && (
-        <ModalLayout onClose={toggleModal} labelledBy="title">
-          <ModalHeader>
-            <Typography fontWeight="bold" id="title">
-              Select icon
-            </Typography>
-          </ModalHeader>
-          <ModalBody>
-            <Flex direction="row" wrap="wrap" gap={2}>
-              {selectedIconLibrary ? (
-                selectableIcons.length > 0 ? (
-                  selectableIcons.map((icon) => (
-                    <Box
-                      key={icon}
-                      variant="secondary"
-                      onClick={() => {
-                        toggleModal();
-                        changeIcon(icon);
-                      }}
-                    >
-                      <IconComponent size={30} icon={icon} />
-                    </Box>
-                  ))
-                ) : (
-                  <Typography variant="pi">No icons available</Typography>
-                )
-              ) : (
-                <Typography variant="pi">No icon library selected</Typography>
-              )}
-            </Flex>
-          </ModalBody>
-          <ModalFooter
-            startActions={
-              <Select
-                id="iconLibrarySelect"
-                minWidth={500}
-                selectButtonTitle="Select icon library"
-                placeholder="Select icon library"
-                required={0}
-                error={error}
-                value={selectedIconLibrary}
-                onChange={setSelectedIconLibrary}
-              >
-                {iconLibraries.map((iconLibrary) => (
-                  <Option key={iconLibrary.id} value={iconLibrary.abbreviation}>
-                    {iconLibrary.name}
-                  </Option>
-                ))}
-              </Select>
-            }
-            endActions={
-              <Button variant="tertiary" onClick={toggleModal}>
-                Close
-              </Button>
-            }
-          />
-        </ModalLayout>
-      )}
-    </>
-  );
+	return (
+		<>
+			<TextInput
+				type="text"
+				label={intlLabel && formatMessage(intlLabel)}
+				placeholder={placeholder && formatMessage(placeholder)}
+				hint={description && formatMessage(description)}
+				disabled={true}
+				onChange={onChange}
+				id={name}
+				name={name}
+				value={value || ""}
+				required={required}
+				error={error}
+				startAction={
+					<FieldAction onClick={toggleModal} style={inputButtonStyle} left="0">
+						{value ? (
+							<IconComponent icon={value} width="1.25rem" height="1.25rem" />
+						) : (
+							<ReactIcons.TbSearch width="1.25rem" height="1.25rem" />
+						)}
+					</FieldAction>
+				}
+				endAction={
+					!!value && (
+						<FieldAction
+							onClick={() => changeIcon("")}
+							style={inputButtonStyle}
+							right="0"
+						>
+							<ReactIcons.TbX />
+						</FieldAction>
+					)
+				}
+			/>
+
+			{isModalVisible && (
+				<ModalLayout
+					onClose={toggleModal}
+					labelledBy="title"
+					width="65vw !important"
+				>
+					<ModalHeader>
+						<Typography fontWeight="bold" id="title">
+							Select icon
+						</Typography>
+					</ModalHeader>
+					<ModalBody
+						minHeight="70vh"
+						maxHeight="unset !important"
+						overflow="hidden"
+					>
+						<TextInput
+							id="icon-search"
+							name="icon-search"
+							label={intlLabel && formatMessage(intlLabel)}
+							value={searchTerm}
+							onChange={(e) => setSearchTerm(e.target.value)}
+							placeholder="Search icons..."
+							margin={"15px"}
+						/>
+						<Grid
+							columnCount={11} // Change to fit your needs
+							rowCount={Math.ceil(selectableIcons.length / 11)} // Change 10 to your column count
+							columnWidth={100} // Change to fit your needs
+							rowHeight={60} // Change to fit your needs
+							width={1150} // Change to fit your needs
+							height={500} // Change to fit your needs
+						>
+							{({ columnIndex, rowIndex, style }) => {
+								const icon = selectableIcons.filter((icon) =>
+									icon.toLowerCase().includes(searchTerm.toLowerCase())
+								)[rowIndex * 11 + columnIndex]; // Change 10 to your column count
+
+								if (!icon) return null;
+
+								return (
+									<Box
+										style={style}
+										width="100%"
+										key={icon}
+										variant="secondary"
+										onClick={() => {
+											toggleModal();
+											changeIcon(icon);
+										}}
+									>
+										<IconComponent size={50} icon={icon} />
+									</Box>
+								);
+							}}
+						</Grid>
+					</ModalBody>
+					<ModalFooter
+						startActions={
+							<Select
+								id="iconLibrarySelect"
+								minWidth={500}
+								selectButtonTitle="Select icon library"
+								placeholder="Select icon library"
+								required={0}
+								error={error}
+								value={selectedIconLibrary}
+								onChange={setSelectedIconLibrary}
+							>
+								{iconLibraries.map((iconLibrary) => (
+									<Option key={iconLibrary.id} value={iconLibrary.abbreviation}>
+										{iconLibrary.name}
+									</Option>
+								))}
+							</Select>
+						}
+						endActions={
+							<Button variant="tertiary" onClick={toggleModal}>
+								Close
+							</Button>
+						}
+					/>
+				</ModalLayout>
+			)}
+		</>
+	);
 };
 
 export default ReactIconsSelector;
